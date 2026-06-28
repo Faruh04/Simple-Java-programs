4fb775628c0bff53fd47d14be96cfba3926b80dc
Faruh04 <gafurjanowf@gmail.com>
2026-06-28 23:12:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+
+public void Main{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static class main(String[] args){
+
+
+
+
+
+
+    }
+        }
